dae23171383fd8cbad57e207f137244682740fbb
Maxwell Lysle <max@lysle.org>
2026-02-03 15:32:01 -0700

    character loop, input checking

--- GuessingGame.java
+++ GuessingGame.java
@@ -8,11 +8,21 @@ public class GuessingGame {
         String word = wordList[index];
         Scanner inputReader = new Scanner(System.in);
         String input;
+        int wordLength = word.length();
         System.out.println(word);
         do {
           System.out.println("Guess word");
           input = inputReader.nextLine();  // Read user input
+          if (input.length() != wordLength) {
+            System.out.println("Wrong word length. Should be " + wordLength + " letters.");
+            continue;
+          }
+          for (int i = 0; i < word.length(); i++){
+            char c1 = word.charAt(i);
+            char c2 = input.charAt(i);
+          }
         } while (!word.equalsIgnoreCase(input));
         inputReader.close();
+
     }
 }
