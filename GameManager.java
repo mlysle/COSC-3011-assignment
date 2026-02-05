d1ebca7a5256e3c491169172a77e752cf2a5c6cb
Ivan Martin <69719490+DojeeDev@users.noreply.github.com>
2026-02-05 09:24:43 -0700

    Got game working in basic state.

--- GameManager.java
+++ GameManager.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class GameManager {
@@ -15,7 +16,22 @@ public class GameManager {
     int numCorrectPosition = 0;
     Scanner inputReader = new Scanner(System.in);
     String input;
+    HashMap<Character, Integer> wordMap = new HashMap<>();
 
+    for (int i = 0; i < word.length(); i++) {
+      char c = word.charAt(i);
+      c = Character.toUpperCase(c);
+      Integer letterCount = wordMap.get(c);
+
+      if (letterCount != null) {
+        wordMap.put(c, letterCount + 1);
+      }
+      else {
+        wordMap.put(c, 1);
+      }
+    }
+
+    System.out.println(wordMap);
     System.out.println(word);
 
     do {
@@ -23,21 +39,33 @@ public class GameManager {
       input = inputReader.nextLine(); 
       numCorrectPosition = 0;
       numCorrect = 0;
+      HashMap<Character, Integer> loopWordMap = wordMap;
 
       if (input.length() != wordLength) {
         System.out.println("Wrong word length. Should be " + wordLength + " letters.");
         continue;
       }
+
       for (int i = 0; i < word.length(); i++) {
         char c1 = word.charAt(i);
         char c2 = input.charAt(i);
         if (Character.toLowerCase(c1) == Character.toLowerCase(c2)) {
           numCorrectPosition++;
         }
+        c2 = Character.toUpperCase(c2);
+        Integer letterCount = wordMap.get(c2);
+        if (letterCount != null) {
+          if (letterCount >= 0) {
+            numCorrect++;
+            loopWordMap.put(c2, letterCount-1);
+          }
+        }
+        
       }
 
-      System.out.println(numCorrectPosition);
+      System.out.println("(" + numCorrect + ", " + numCorrectPosition + ")");
     } while (!word.equalsIgnoreCase(input));
     inputReader.close();
+    System.out.println("Success");
   }
 }
